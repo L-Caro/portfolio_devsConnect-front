897f9551ffeed37707a3692082df988a9e351070
L-Caro <lionelcaro74@gmail.com>
2023-06-08 13:24:28 +0200

    design Login + responsive OK

--- src/components/Form/Login/Login.tsx
+++ src/components/Form/Login/Login.tsx
@@ -1,15 +1,67 @@
+import { useState, useRef, useEffect } from 'react';
+
+// Permet de relancer le rendu de ce composant à chaque fois que le state de la modale change
+import { useAppDispatch } from '../../../hook/redux';
+
+// Actions du reducer
+import { toggleModalLogin } from '../../../store/reducer/log';
+
 // Composants
-import Title from '../../App/Layout/Header/Title/Title';
 import Input from '../Input';
 
 // Styles
 import './style.scss';
 
 function Login() {
+  const modalRef = useRef(null);
+
+  // Dispatch
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        // Clic en dehors de la modale
+        dispatch(toggleModalLogin());
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const handleLogin = () => {
+    // On dispatch l'action qui va gérer l'ouverture de la modale
+    dispatch(toggleModalLogin());
+  };
+
+  // * Une div n'est pas un element clickable
+  // * Fonction d’accessibilité pour le clavier.
+  // * Si la touche enter ou espace est pressée, on appelle la fonction handleClick()
+  const handleLoginKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === 'enter' || event.key === ' ') {
+      handleLogin();
+    }
+  };
+
   return (
     <div className="Login">
-      <div className="Login--container">
-        <h2 className="Login--title">Connexion</h2>
+      <div className="Login--container" ref={modalRef}>
+        <div className="Login--container--head">
+          <h2 className="Login--title">Connexion</h2>
+          <div
+            className="Login--close"
+            role="button"
+            tabIndex={0}
+            onClick={handleLogin}
+            onKeyDown={handleLoginKeyDown}
+          >
+            X
+          </div>
+        </div>
         <form className="Login--form">
           <Input
             name="email"
@@ -24,7 +76,7 @@ function Login() {
             className="Login--inputText"
           />
 
-          <button type="submit" className="Login--container--form--submit">
+          <button type="submit" className="Login--form--submit">
             Se connecter
           </button>
         </form>
