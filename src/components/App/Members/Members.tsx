d8ed2810d093d058c7f07ad92852a28f12ed9f89
L-Caro <lionelcaro74@gmail.com>
2023-06-09 20:05:16 +0200

    refacto css signin

--- src/components/App/Members/Members.tsx
+++ src/components/App/Members/Members.tsx
@@ -11,6 +11,118 @@ function Members() {
         <CardMember />
         <CardMember />
         <CardMember />
+        <section className="CardMember--card">
+          <div className="CardMember--card--header">
+            <div className="CardMember--card--header--text">
+              <h3>Lorraine Ipsum</h3>
+              <p className="">Disponible</p>
+            </div>
+            <img src="/images/profil/profil.svg" alt="vite" />
+          </div>
+          <div className="CardMember--card--body">
+            <p className="CardMember--card--body--text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+              alias debitis magnam quo et laudantium temporibus dignissimos
+              delectus dolor, iure repellendus nam possimus ullam?
+            </p>
+            <p className="CardMember--card--body--text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+              alias debitis magnam quo et laudantium temporibus dignissimos
+              delectus dolor, iure repellendus nam possimus ullam?
+            </p>
+            <p className="CardMember--card--body--text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+              alias debitis magnam quo et laudantium temporibus dignissimos
+              delectus dolor, iure repellendus nam possimus ullam?
+            </p>
+          </div>
+          <div className="CardMember--card--footer">
+            <p>Projets :</p>
+            <ul className="CardMember--card--footer--list">
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Amet consectetur adipisicing elit.</li>
+              <li>Animi quis magni dolore iure?</li>
+            </ul>
+          </div>
+        </section>{' '}
+        <section className="CardMember--card">
+          <div className="CardMember--card--header">
+            <div className="CardMember--card--header--text">
+              <h3>Lorraine Ipsum</h3>
+              <p className="">Disponible</p>
+            </div>
+            <img src="/images/profil/profil.svg" alt="vite" />
+          </div>
+          <div className="CardMember--card--body">
+            <p className="CardMember--card--body--text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+              alias debitis magnam quo et laudantium temporibus dignissimos
+              delectus dolor, iure repellendus nam possimus ullam?
+            </p>
+          </div>
+          <div className="CardMember--card--footer">
+            <p>Projets :</p>
+            <ul className="CardMember--card--footer--list">
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Amet consectetur adipisicing elit.</li>
+              <li>Animi quis magni dolore iure?</li>
+            </ul>{' '}
+            <ul className="CardMember--card--footer--list">
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Amet consectetur adipisicing elit.</li>
+              <li>Animi quis magni dolore iure?</li>
+            </ul>{' '}
+            <ul className="CardMember--card--footer--list">
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Amet consectetur adipisicing elit.</li>
+              <li>Animi quis magni dolore iure?</li>
+            </ul>{' '}
+            <ul className="CardMember--card--footer--list">
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Amet consectetur adipisicing elit.</li>
+              <li>Animi quis magni dolore iure?</li>
+            </ul>
+          </div>
+        </section>
+        <section className="CardMember--card">
+          <div className="CardMember--card--header">
+            <div className="CardMember--card--header--text">
+              <h3>Lorraine Ipsum</h3>
+              <p className="">Disponible</p>
+            </div>
+            <img src="/images/profil/profil.svg" alt="vite" />
+          </div>
+          <div className="CardMember--card--body">
+            <p className="CardMember--card--body--text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+              alias debitis magnam quo et laudantium temporibus dignissimos
+              delectus dolor, iure repellendus nam possimus ullam?
+            </p>
+          </div>
+          <div className="CardMember--card--footer">
+            <p>Projets :</p>
+            <ul className="CardMember--card--footer--list">
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Amet consectetur adipisicing elit.</li>
+              <li>Animi quis magni dolore iure?</li>
+            </ul>{' '}
+            <ul className="CardMember--card--footer--list">
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Amet consectetur adipisicing elit.</li>
+              <li>Animi quis magni dolore iure?</li>
+            </ul>{' '}
+            <ul className="CardMember--card--footer--list">
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Amet consectetur adipisicing elit.</li>
+              <li>Animi quis magni dolore iure?</li>
+            </ul>{' '}
+            <ul className="CardMember--card--footer--list">
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Amet consectetur adipisicing elit.</li>
+              <li>Animi quis magni dolore iure?</li>
+            </ul>
+          </div>
+        </section>
       </div>
     </div>
   );
