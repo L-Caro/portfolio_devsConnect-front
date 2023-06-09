f381b65276c8180c87517a499c0c78f803c4584a
L-Caro <lionelcaro74@gmail.com>
2023-06-09 22:43:54 +0200

    fin du css/responsive members/cardmembers

--- src/components/App/Members/CardMember.tsx
+++ src/components/App/Members/CardMember.tsx
@@ -16,16 +16,34 @@ function CardMember() {
           <p className="CardMember--card--body--text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio alias
             debitis magnam quo et laudantium temporibus dignissimos delectus
-            dolor, iure repellendus nam possimus ullam?
+            dolor, iure repellendus nam possimus ullam? Lorem ipsum dolor sit
+            amet, consectetur adipisicing elit. Vero, perspiciatis! Sunt illum
+            incidunt ipsam aperiam, corporis vitae esse odio soluta molestias
+            officia impedit obcaecati reprehenderit dolores deleniti minus alias
+            saepe, voluptatibus nesciunt enim nihil minima dolorum culpa quidem
+            autem. Alias.
           </p>
           <div className="CardMember--card--body--technos">
+            {/*     
+            //! Map() pour limiter à 5 l'affichage des technos de l'appel API
+            <div>
+              {data.map((item, index) => {
+                if (index < 5) {
+                  return <li key={index}>{item}</li>;
+                }
+                return null;
+              })}
+            </div> 
+            */}
             <img src={technosPath.html} />
             <img src={technosPath.react} />
             <img src={technosPath.css} />
+            <img src={technosPath.javascript} />
+            <img src={technosPath.nodejs} />
           </div>
         </div>
         <div className="CardMember--card--footer">
-          <p>Projets :</p>
+          <h4>Projets :</h4>
           <ul className="CardMember--card--footer--list">
             <li>Lorem ipsum dolor sit amet.</li>
             <li>Amet consectetur adipisicing elit.</li>
