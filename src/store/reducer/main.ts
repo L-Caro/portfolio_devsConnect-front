79cb33d59ac29258c55e66b5e2988bfc4d6c8086
L-Caro <lionelcaro74@gmail.com>
2023-06-21 23:53:36 +0200

    refacto reducer main

--- src/store/reducer/main.ts
+++ src/store/reducer/main.ts
@@ -1,24 +1,23 @@
-// ? Si utilisation d'API, décommenter lignes 5 et 20
-// ? Typer les fonctions synchrone et asynchrone (`<>` ligne 17 et 20)
-// ? Remplacer les `state1` et `action1` par les noms de vos states et actions
-
-// import { createAsyncThunk } from '@reduxjs/toolkit';
+// ? Librairies
 import { createAction, createReducer } from '@reduxjs/toolkit';
 
+// ? Typage local
 interface MainState {
   windowWidth: number;
 }
 
+// ? InitialState
 const initialState: MainState = {
   windowWidth: window.innerWidth,
 };
 
-// Fonctions synchrone
+// ? Fonctions synchrone
 export const resizeWindow = createAction('main/resizeWindow');
 
+// ? Reducer
 const mainReducer = createReducer(initialState, (builder) => {
   builder.addCase(resizeWindow, (state) => {
-    state.windowWidth = window.innerWidth;
+    state.windowWidth = window.innerWidth; // Modifie la valeur du state
   });
 });
 
