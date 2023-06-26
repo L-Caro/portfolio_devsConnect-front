8196d45a905d258a4712f789341d70cbaf4e99f0
DanglotClement <clementdanglot@gmail.com>
2023-06-26 17:46:51 +0200

    postuler et supprimer participant ok

--- src/store/reducer/participants.ts
+++ src/store/reducer/participants.ts
@@ -8,7 +8,6 @@ export const addParticipantToProject = createAsyncThunk(
       const { data } = await axiosInstance.post(
         `/api/projects/${projectId}/user/${userId}`
       );
-
       return data;
     } catch (error) {
       console.log(error);
@@ -30,22 +29,65 @@ export const deleteParticipantFromProject = createAsyncThunk(
   }
 );
 
-const participantsReducer = createReducer([], (builder) => {
-  builder
-    .addCase(addParticipantToProject.pending, (state) => {})
-    .addCase(addParticipantToProject.fulfilled, (state, action) => {
-      state.push(action.payload);
-    })
-    .addCase(addParticipantToProject.rejected, (state) => {})
-    .addCase(deleteParticipantFromProject.pending, (state) => {})
-    .addCase(deleteParticipantFromProject.fulfilled, (state, action) => {
-      const { userId, projectId } = action.payload;
-      return state.filter(
-        (participant) =>
-          participant.userId !== userId || participant.projectId !== projectId
+export const validatePendingParticipant = createAsyncThunk(
+  'participants/validatePendingParticipant',
+  async ({ projectId, userId }) => {
+    try {
+      const { data } = await axiosInstance.put(
+        `/api/projects/${projectId}/user/${userId}/validate`
       );
-    })
-    .addCase(deleteParticipantFromProject.rejected, (state) => {});
-});
+      return data;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+);
+
+const participantsReducer = createReducer(
+  { participants: [], pendingParticipants: [] },
+  (builder) => {
+    builder
+      .addCase(addParticipantToProject.fulfilled, (state, action) => {
+        const { userId } = action.payload;
+        if (
+          state.pendingParticipants.find(
+            (participant) => participant.userId === userId
+          )
+        ) {
+          state.pendingParticipants = state.pendingParticipants.filter(
+            (participant) => participant.userId !== userId
+          );
+        } else {
+          state.participants.push(action.payload);
+        }
+      })
+      .addCase(deleteParticipantFromProject.fulfilled, (state, action) => {
+        const { userId, projectId } = action.payload;
+        state.participants = state.participants.filter(
+          (participant) =>
+            participant.userId !== userId || participant.projectId !== projectId
+        );
+        state.pendingParticipants = state.pendingParticipants.filter(
+          (participant) =>
+            participant.userId !== userId || participant.projectId !== projectId
+        );
+      })
+      .addCase(validatePendingParticipant.fulfilled, (state, action) => {
+        const { userId } = action.payload;
+        const pendingParticipantIndex = state.pendingParticipants.findIndex(
+          (participant) => participant.userId === userId
+        );
+        if (pendingParticipantIndex !== -1) {
+          const [participant] = state.pendingParticipants.splice(
+            pendingParticipantIndex,
+            1
+          );
+
+          state.participants.push(participant);
+        }
+      });
+  }
+);
 
 export default participantsReducer;
