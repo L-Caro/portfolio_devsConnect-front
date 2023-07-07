7d0be53fa46eabd1f23e65a848f639ffdf658249
L-Caro <lionelcaro74@gmail.com>
2023-07-07 10:42:30 +0200

    intégration d'un carousel pour les projets du membre

--- src/components/App/Members/OneMember/OneMember.tsx
+++ src/components/App/Members/OneMember/OneMember.tsx
@@ -2,6 +2,9 @@
 import { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Audio } from 'react-loader-spinner';
+import Carousel from 'react-multi-carousel';
+import { responsive } from '../../../../utils/CustomCarousel';
+import 'react-multi-carousel/lib/styles.css';
 
 import { useAppSelector, useAppDispatch } from '../../../../hook/redux';
 
@@ -140,28 +143,45 @@ function OneMember() {
             <div className="OneMember--secondField--description">
               {member.description}
             </div>
-            {/** //! Affichage des projets
-             * @param {Array} member.projects - Liste des projets
-             * Si member.projects existe et qu'il y a au moins un projet
-             * On map sur la liste des projets
-             */}
-            {member.projects && member.projects.length > 0 && (
-              <div className="OneMember--secondField--projects">
-                <h4 className="OneMember--secondField--projects--title">
-                  Projets réalisés
-                </h4>
-                {/** //! ProjectCard.tsx
-                 * @param {Object} projectID - Données du projet
-                 * @param {Number} key - Clé unique pour chaque projet
-                 * On envoie au composant ProjectCard les données de chaque projet
-                 * et une clé unique
-                 */}
+            <div className="OneMember--secondField--projects">
+              <h4 className="OneMember--secondField--projects--title">
+                Projets réalisés
+              </h4>
+              {member.projects && member.projects.length > 0 && (
+                <Carousel
+                  // ? Paramètres du carousel
+                  responsive={responsive} // Gère des paramètres spécifiques en fonction de la taille de l'écran
+                  swipeable // Swipe sur mobile (false === interdit)
+                  draggable={false} // Drag sur mobile (false === interdit)
+                  // ? Affichage/Position des éléments
+                  // centerMode // Affiche partiellement les cartes gauches et droites
+                  showDots // Affiche les points de navigation
+                  renderDotsOutside // Affiche les points de navigation en dehors du carousel
+                  removeArrowOnDeviceType="mobile" // Supprime les flèches de navigation sur mobile
+                  renderButtonGroupOutside={false} // Affiche les boutons de navigation en dehors du carousel
+                  // ? Animations
+                  // rewind // Permet de revenir au début de la liste après la dernière carte
+                  // rewindWithAnimation // Revenir au début de la liste avec une animation
+                  infinite // Permet de revenir au début de la liste
+                  autoPlay // Défilement automatique
+                  autoPlaySpeed={7000} // Vitesse de défilement (temps entre chaque slide)
+                  customTransition="transform 2000ms ease-in-out" // Transition entre chaque slide
+                  shouldResetAutoplay // Reset l'autoplay à chaque interaction
+                  transitionDuration={2000}
+                >
+                  {/** //! ProjectCard.tsx
+                   * @param {Object} projectID - Données du projet
+                   * @param {Number} key - Clé unique pour chaque projet
+                   * On envoie au composant ProjectCard les données de chaque projet
+                   * et une clé unique
+                   */}
 
-                {member.projects.map((project) => (
-                  <ProjectCard key={project.id} projectID={project} />
-                ))}
-              </div>
-            )}
+                  {member.projects.map((project) => (
+                    <ProjectCard key={project.id} projectID={project} />
+                  ))}
+                </Carousel>
+              )}{' '}
+            </div>
           </div>
         </div>
       </div>
@@ -170,3 +190,9 @@ function OneMember() {
 }
 
 export default OneMember;
+
+//  {/** //! Affichage des projets
+//              * @param {Array} member.projects - Liste des projets
+//              * Si member.projects existe et qu'il y a au moins un projet
+//              * On map sur la liste des projets
+//              */}
---
+++ src/utils/CustomCarousel.js
@@ -0,0 +1,17 @@
+export const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 501 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+  mobile: {
+    // arrow: true,
+    breakpoint: { max: 499, min: 0 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
+
+export const CustomCarousel = ({ children, deviceType }) => {
+  console.log(deviceType);
+};
